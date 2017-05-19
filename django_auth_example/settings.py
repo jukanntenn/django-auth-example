d37f9ae354310d3d85a7c17817c5bed4efbc1d6c
yangxg <zmrenwu@163.com>
2017-05-19 14:32:08 +0800

    Django 用户认证系统：拓展 User 模型

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -113,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
